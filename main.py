a54501f8419ec09c32d511900d8a62c99c4026d7
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-07-30 11:11:46 +0300

    Rendered snake body to screen

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ GAME_WIDTH = 500
 GRID_SIZE = 20
 BACKGROUND_COLOR = '#073b4c'
 FOOD_COLOR='#ef476f'
+BODY_PARTS = 3
+SNAKE_COLOR = '#06d6a0'
 
 
 class Food:
@@ -14,6 +16,20 @@ class Food:
         y = randint(0, (GAME_HEIGHT / GRID_SIZE) - 1) * GRID_SIZE
         self.coordinates = [x, y]
         canvas.create_rectangle(x, y, x + GRID_SIZE, y + GRID_SIZE, fill=FOOD_COLOR, tags='food', width=0)
+
+
+class Snake:
+    def __init__(self):
+        self.size = BODY_PARTS
+        self.coordinates = []
+        self.squares = []
+
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+        
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + GRID_SIZE, y + GRID_SIZE, fill=SNAKE_COLOR, tags='snake', width=0, )
+            self.squares.append(square)
 
 
 window = Tk()
@@ -26,6 +42,7 @@ canvas.pack()
 window.update()
 
 food = Food()
+snake = Snake()
 
 if __name__ == '__main__':
     window.mainloop()
