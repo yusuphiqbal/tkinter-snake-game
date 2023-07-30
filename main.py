aafd3fd9fd02f1c74d65a4f2ef283cf3f0b5e6d1
Yusuph Iqbal <yusuphiqbal21@gmail.com>
2023-07-30 11:52:13 +0300

    Checked for collisions with screen and body

--- main.py
+++ main.py
@@ -35,6 +35,23 @@ class Snake:
             self.squares.append(square)
 
 
+def check_collisions(snake):
+    x, y = snake.coordinates[0]
+
+    if x < 0 or x >= GAME_WIDTH or y < 0 or y >= GAME_HEIGHT:
+        return True
+
+    for body_part in snake.coordinates[1:]:
+        if x == body_part[0] and y == body_part[1]:
+            return True
+
+    return False
+
+
+def game_over():
+    pass
+
+
 def next_turn(snake, food):
     x, y = snake.coordinates[0]
 
@@ -59,9 +76,10 @@ def next_turn(snake, food):
         canvas.delete(snake.squares[-1])
         del snake.squares[-1]
 
-    
-
-    window.after(REFRESH_IN_MS, next_turn, snake, food)
+    if check_collisions(snake):
+        game_over()
+    else:
+        window.after(REFRESH_IN_MS, next_turn, snake, food)
 
 
 def change_direction(new_direction):
